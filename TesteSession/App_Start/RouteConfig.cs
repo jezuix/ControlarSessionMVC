d2d2f62c417ffa525dc153929e07740290073fa9
Rodolpho Rangel de Barros <mr_condor_@hotmail.com>
2018-06-18 22:57:10 -0300

    Preparo Inicial.

--- TesteSession/App_Start/RouteConfig.cs
+++ TesteSession/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using TesteSession.Configuracao;
 
 namespace TesteSession
 {
@@ -13,11 +14,14 @@ namespace TesteSession
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
+            routes.Add("Default", new GuidRoute("{controller}/{action}/{id}", new { controller = "Home", action = "Index", guid = "", id = UrlParameter.Optional }));
+            routes.Add("GuidRoute", new GuidRoute("g/{guid}/{controller}/{action}/{id}", new { controller = "Home", action = "Index", guid = "", id = UrlParameter.Optional }));
         }
     }
 }
---
+++ TesteSession/Configuracao/ControllerExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace TesteSession.Configuracao
+{
+    public static class ControllerExtensionMethods
+    {
+        public static string GetGuid(this Controller controller)
+        {
+            return controller.RouteData.Values["guid"].ToString();
+        }
+        public static void SetGuidSession(this Controller controller, string name, object value)
+        {
+            controller.Session[controller.GetGuid() + "_" + name] = value;
+        }
+        public static object GetGuidSession(this Controller controller, string name)
+        {
+            return controller.Session[controller.GetGuid() + "_" + name];
+        }
+    }
+}
